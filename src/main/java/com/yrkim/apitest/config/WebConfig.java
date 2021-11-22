a165672e746a809ac52cc59ec69d99909efe8ab2
yeongroke <chfaos120@naver.com>
2021-11-22 23:19:19 +0900

    [CNF] WebConfig cors 어노테이션추가 - httpmethod 로직 변경

--- src/main/java/com/yrkim/apitest/config/WebConfig.java
+++ src/main/java/com/yrkim/apitest/config/WebConfig.java
@@ -1,15 +1,22 @@
 package com.yrkim.apitest.config;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 @Slf4j
 @Configuration
+@RequiredArgsConstructor
+@CrossOrigin
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
@@ -22,12 +29,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods(
-                        HttpMethod.GET.name(),
-                        HttpMethod.HEAD.name(),
-                        HttpMethod.POST.name(),
-                        HttpMethod.PUT.name(),
-                        HttpMethod.DELETE.name()
-                );
+                .allowedMethods(Arrays.asList(HttpMethod.values()).stream()
+                        .map(method -> method.name()).collect(Collectors.toList()).stream()
+                        .toArray(size -> new String[size])).allowCredentials(false).maxAge(3600);
     }
 }
